00fb3f2c4ba933677e300938d903bfdde4d433c4
Ibrahim <azh.ibarhim7@gmail.com>
2026-04-04 07:39:22 +0100

    Separate the connection configuration from the task  model.

--- task_Models/db.php
+++ task_Models/db.php
@@ -1,30 +1,6 @@
 <?php
 
-
-class Connexion{
-
-    private static $_host = "localhost";
-    private static $_user = "root";
-    private static $_pwd = "";
-    private static $_dbname = "task_db";
-
-    public static function getPDO()
-    {
-        try {
-            $pdo = new PDO(
-                "mysql:host=" . self::$_host . ";dbname=" . self::$_dbname ,
-                self::$_user,
-                self::$_pwd
-            );
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $pdo;
-        } catch (PDOException $e) {
-            die("Connection failed: " . $e->getMessage());
-        }
-    }
-}
-
-
+require '../config.php';
 
 class Task {
     private $pdo;
